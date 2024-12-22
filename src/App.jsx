d04097faeea8ac7b2abf535bf4dca9301bbaa358
Ryxn-sudo <23ntucsfl1280@student.ntu.edu.pk>
2024-12-22 14:57:33 +0500

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Routes , Route} from 'react-router-dom'
 import Login from './Pages/Login/Login'
-import OtpLogin from './components/Otp Login/Otplogin'
+
 
 const App = () => {
   return (
    <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/otp" element={<OtpLogin />}/>
+     
     </Routes>
   )
 }
