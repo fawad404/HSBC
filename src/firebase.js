2071ab0f2c305cc73b01bb3b331eb44dfb88f69b
fawad404 <159186424+fawad404@users.noreply.github.com>
2024-12-25 16:39:21 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCwZOeh_ohz9bysr7VhKbBUnR4PP330S-E",
   authDomain: "otp-verificaiton-c345d.firebaseapp.com",
   projectId: "otp-verificaiton-c345d",
-  storageBucket: "otp-verificaiton-c345d.appspot.com",
+  storageBucket: "otp-verificaiton-c345d.firebasestorage.app",
   messagingSenderId: "333081282673",
   appId: "1:333081282673:web:4cd0ba1f1251482eecf3d0"
 };
