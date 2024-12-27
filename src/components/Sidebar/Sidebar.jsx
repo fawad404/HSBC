1e485c4d2aa6cb40d2ee2077d2b898af0b548e36
fawad404 <fawadanxari31@gmail.com>
2024-12-28 01:17:41 +0500

    color-changed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -73,7 +73,7 @@ export default function Sidebar({ isOpen, setIsOpen }) {
             </p>
             <button
               onClick={() => setShowPopup(false)}
-              className="px-4 py-2 bg-[#DB104F] text-white rounded-md hover:bg-[#83000A] transition-colors"
+              className="px-4 py-2 bg-[#db0011] text-white rounded-md hover:bg-[#83000A] transition-colors"
             >
               OK
             </button>
